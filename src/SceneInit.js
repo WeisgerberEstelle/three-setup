5461f48b9bc90eac54cde567706de5086e1910dc
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2022-04-05 16:42:37 +0200

    add texture, light, gui, background

--- src/SceneInit.js
+++ src/SceneInit.js
@@ -1,78 +1,90 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "three/examples/jsm/libs/stats.module";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
-class SceneInit {
+export default class SceneInit {
   constructor(canvasId) {
-
-    /// NOTE: Camera params;
-    this.fov = 45;
-    this.canvasId = canvasId;
-
-    //// NOTE: Core components to initialize Three.js app.
+    // NOTE: Core components to initialize Three.js app.
     this.scene = undefined;
     this.camera = undefined;
     this.renderer = undefined;
 
-    // NOTE : additional components
+    // NOTE: Camera params;
+    this.fov = 45;
+    this.nearPlane = 1;
+    this.farPlane = 1000;
+    this.canvasId = canvasId;
+
+    // NOTE: Additional components.
     this.clock = undefined;
-    this.controls = undefined;
     this.stats = undefined;
+    this.controls = undefined;
 
     // NOTE: Lighting is basically required.
-    this.ambientLight = undefined;
     this.spotLight = undefined;
+    this.ambientLight = undefined;
   }
 
   initialize() {
+    this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
-      50,
+      this.fov,
       window.innerWidth / window.innerHeight,
       1,
       1000
     );
-    this.camera.position.z = 48;
+    this.camera.position.z = 16;
 
-    
-    this.scene = new THREE.Scene();
-
-    //canvas
-    const canvas = document.getElementById("myThreeJsCanvas");
+    // NOTE: Specify a canvas which is already created in the HTML.
+    const canvas = document.getElementById(this.canvasId);
     this.renderer = new THREE.WebGLRenderer({
       canvas,
+      // NOTE: Anti-aliasing smooths out the edges.
       antialias: true,
     });
-
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // enable this for shadows
+    this.renderer.shadowMap.enabled = true;
     document.body.appendChild(this.renderer.domElement);
 
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.clock = new THREE.Clock();
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.stats = Stats();
     document.body.appendChild(this.stats.dom);
 
-    //lights
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    this.scene.add(ambientLight);
+    // HIDE LIGHTING FOR DEMO.
+    // ambient light which is for the whole scene
+    // this.ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    // this.ambientLight.castShadow = true;
+    // this.scene.add(this.ambientLight);
+    // spot light which is illuminating the chart directly
+    // this.spotLight = new THREE.SpotLight(0xffffff, 1);
+    // this.spotLight.castShadow = true;
+    // this.spotLight.position.set(0, 64, 32);
+    // this.scene.add(this.spotLight);
 
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    this.scene.add(spotLight);
-
+    // if window resizes
     window.addEventListener('resize', () => this.onWindowResize(), false);
 
+    // NOTE: Load space background.
+    // this.loader = new THREE.TextureLoader();
+    // this.scene.background = this.loader.load('./pics/space.jpeg');
+
+    // NOTE: Declare uniforms to pass into glsl shaders.
+    // this.uniforms = {
+    //   u_time: { type: 'f', value: 1.0 },
+    //   colorB: { type: 'vec3', value: new THREE.Color(0xfff000) },
+    //   colorA: { type: 'vec3', value: new THREE.Color(0xffffff) },
+    // };
   }
 
-  animate(){
-    //boxMesh.rotation.x += 0.01;
-    //boxMesh.rotation.y += 0.01;
+  animate() {
+    // NOTE: Window is implied.
+    // requestAnimationFrame(this.animate.bind(this));
     window.requestAnimationFrame(this.animate.bind(this));
     this.render();
     this.stats.update();
     this.controls.update();
-    
   }
 
   render() {
@@ -86,7 +98,4 @@ class SceneInit {
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
-
 }
-
-export default SceneInit;
